e184ea067a19f4e9c8c2e4772e04089a42ea922e
Kristoffer Hansen <kriz@motnok.dk>
2010-10-23 14:44:35 -0700

    More log messages

--- controls.js
+++ controls.js
@@ -5,6 +5,20 @@ if(boxee.getVersion() > 1.8)
   boxee.setCanPause(true);
 }
 
+boxee.log("--------------");
+boxee.log("--------------");
+boxee.log("--------------");
+boxee.log("--------------");
+boxee.log("--------------");
+boxee.log("--------------");
+boxee.log("--------------");
+boxee.log("--------------");
+boxee.log("--------------");
+boxee.log("--------------");
+boxee.log("--------------");
+boxee.log("--------------");
+boxee.log("--------------");
+boxee.log("--------------");
 boxee.log(browser.execute("login = TV2.Sputnik.userIsLoggedIn() == true ? 0 : 1");
 
 boxee.onPlay = function()
