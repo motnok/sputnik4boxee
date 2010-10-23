938adefe361889c908d9975395505acea6ad5922
Kristoffer Hansen <kriz@motnok.dk>
2010-10-23 14:41:38 -0700

    Testing browser execute

--- controls.js
+++ controls.js
@@ -5,6 +5,8 @@ if(boxee.getVersion() > 1.8)
   boxee.setCanPause(true);
 }
 
+boxee.log(browser.execute("login = TV2.Sputnik.userIsLoggedIn() == true ? 0 : 1");
+
 boxee.onPlay = function()
 {
 	boxee.runInBrowser('document.getElementById("player").Content.ScriptableMembers.PlayVideo();');
